25251cb31a4500d0263dac71fe11f6519994ff98
Salwasalsabilaa <azhar.salwa.ft20@mail.umy.ac.id>
2022-10-30 19:43:02 +0700

    second commit membuat controller perhitungan jumlah belanja dan diskon

---
+++ src/main/java/TokoKue/TokoKue/tablecontroller.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TokoKue.TokoKue;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Inspiron
+ */
+class tablecontroller {
+        public int gethitung(int jumlah, int harga) {
+        int hargatotal;
+        int total = harga*jumlah;
+        
+        if(total<10000){
+            hargatotal = total - (total * 0);
+        }
+        else if ((total > 10000) && (total < 50000)){
+            hargatotal = total - (total * 5/100);
+        }
+        else{
+            hargatotal = total - (total * 10/100);
+        }
+        return hargatotal;
+    }
+    
+    public int getharga(int jumlah, int harga){
+        int total = harga*jumlah;
+        int totalDiskon;
+        
+        if(total < 10000){
+            totalDiskon = 0;
+        }
+        else if((total > 10000)&&(total < 50000)){
+            totalDiskon = 5;
+        }
+        else{
+            totalDiskon = 10;
+        }
+        return totalDiskon;
+    }
+}
