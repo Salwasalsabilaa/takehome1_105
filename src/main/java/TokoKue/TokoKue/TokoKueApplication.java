ea747a8979e27ab08d3874940e0b552438b23a67
Salwasalsabilaa <azhar.salwa.ft20@mail.umy.ac.id>
2022-10-30 19:24:13 +0700

    first commit membuat index html tampilan input data belanja

---
+++ src/main/java/TokoKue/TokoKue/TokoKueApplication.java
@@ -0,0 +1,13 @@
+package TokoKue.TokoKue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoKueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoKueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TokoKue/TokoKue/TokoKueApplicationTests.java
@@ -0,0 +1,13 @@
+package TokoKue.TokoKue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoKueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
